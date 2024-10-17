0fb6bbb841336352a5cd4b5ee34cd921dab41a28
cho1133 <ryeorkrhtlvek123@gmail.com>
2024-10-17 10:13:38 +0900

    Feat: streamlit-main black 적용

--- main.py
+++ main.py
@@ -1,57 +1,31 @@
 import argparse
 from sql_rag import few_shot, cardinaliy
 
+
 def get_config():
     # parser 생성
     parser = argparse.ArgumentParser(
         prog="Get Config",
     )
-    
-    parser.add_argument(
-        "--few-shot-path",
-        default="./few_shots.json",
-        type=str
-    )
-    
-    parser.add_argument(
-        "--model-name",
-        default="gpt-4o-mini",
-        type=str
-    )
-    
-    parser.add_argument(
-        "--host",
-        default="34.173.158.39",
-        type=str
-    )
-    
-    parser.add_argument(
-        "--port",
-        default="3306",
-        type=str
-    )
-    
-    parser.add_argument(
-        "--username",
-        default="Ssac",
-        type=str
-    )
-    
-    parser.add_argument(
-        "--password",
-        default="test1234",
-        type=str
-    )
-    
-    parser.add_argument(
-        "--database-schema",
-        default="classicmodels",
-        type=str
-    )
-    
+
+    parser.add_argument("--few-shot-path", default="./few_shots.json", type=str)
+
+    parser.add_argument("--model-name", default="gpt-4o-mini", type=str)
+
+    parser.add_argument("--host", default="34.173.158.39", type=str)
+
+    parser.add_argument("--port", default="3306", type=str)
+
+    parser.add_argument("--username", default="Ssac", type=str)
+
+    parser.add_argument("--password", default="test1234", type=str)
+
+    parser.add_argument("--database-schema", default="classicmodels", type=str)
+
     config = parser.parse_args()
 
     return config
+
 
 if __name__ == "__main__":
     config = get_config()
--- streamlit_main.py
+++ streamlit_main.py
@@ -22,7 +22,7 @@ chain = create_sql_query_chain(llm, db)
 
 
 # 세션 상태 초기화
-if 'conversation_history' not in st.session_state:
+if "conversation_history" not in st.session_state:
     st.session_state.conversation_history = []
 
 # SQL agents 사용
@@ -37,21 +37,18 @@ user_input = st.text_input("데이터에 대해 질문하세요요:")
 if st.button("쿼리 생성 및 실행"):
     # SQL 에이전트를 통해 쿼리 생성 및 실행
     response = agent_executor.invoke({"input": user_input})
-    
+
     # response의 전체 내용 확인 (디버깅 목적)
     st.write("Response 전체 내용:")
     st.write(response)
-    
-    # 대화 기록에 추가
-    st.session_state.conversation_history.append({
-        'question': user_input,
-        'response' : response
 
-    })
+    # 대화 기록에 추가
+    st.session_state.conversation_history.append(
+        {"question": user_input, "response": response}
+    )
 
 # 대화 기록 표시
 st.subheader("대화 기록록")
 for entry in st.session_state.conversation_history:
     st.write(f"질문: {entry['question']}")
     st.write(f"Response 전체 내용: {entry['response']}")
-
