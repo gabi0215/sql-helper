d751ba32339d29b7211f289e2e5f29f3aa4b80de
pakyeon <hiyo2044@gmail.com>
2024-10-21 03:58:14 +0900

    REFACTOR:sql_rag.py 묘듈화 STYLE:few_shots.json 한글화, Delete:main.py DB관련 parser 삭제

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
-from sql_rag import few_shot, cardinaliy
-
+from sql_rag import few_shot, cardinality
 
 def get_config():
     # parser 생성
@@ -12,24 +11,14 @@ def get_config():
 
     parser.add_argument("--model-name", default="gpt-4o-mini", type=str)
 
-    parser.add_argument("--host", default="34.173.158.39", type=str)
-
-    parser.add_argument("--port", default="3306", type=str)
-
-    parser.add_argument("--username", default="Ssac", type=str)
-
-    parser.add_argument("--password", default="test1234", type=str)
-
-    parser.add_argument("--database-schema", default="classicmodels", type=str)
-
     config = parser.parse_args()
 
     return config
 
-
 if __name__ == "__main__":
+    # argparser 불러오기
     config = get_config()
-    agent = cardinaliy(config)
-    agent.invoke("다이안의 이메일을 알려줘")
+    agent = cardinality(config)
+    agent.invoke("다이안의 주소를 알려줘")
     # agent = few_shot()
     # agent.invoke("직원이 몇 명이야?")
