d6fbd60e1050934913c6e57abcd997271280c971
pakyeon <hiyo2044@gmail.com>
2024-11-17 05:02:19 +0900

    Fix:context 전처리 부분 수정

--- langgraph_/task.py
+++ langgraph_/task.py
@@ -220,7 +220,7 @@ def extract_context(user_question: str, table_contexts: List[str]) -> List[int]:
 
     structured_llm = llm.with_structured_output(context_list)
     context = ""
-    for idx, table_info in enumerate(table_contexts):
+    for table_info in table_contexts:
         context += f"{table_info}\n\n"
 
     chain = prompt | structured_llm
