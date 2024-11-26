c89454cdeda79d697388f63ae80b85186fc5289e
joonavel <joonavel@gmail.com>
2024-11-26 23:19:29 +0900

    Feat: Front-Back Connection

--- backend/langgraph_/graph.py
+++ backend/langgraph_/graph.py
@@ -127,3 +127,44 @@ def make_graph_for_test() -> CompiledStateGraph:
     app = workflow.compile(checkpointer=memory)
 
     return app
+
+
+def multiturn_test():
+    workflow = StateGraph(GraphState)
+
+    workflow.add_node("question_evaluation", question_evaluation)  # 질문 평가
+    workflow.add_node("general_conversation", non_sql_conversation)  # 일반적인 대화
+    workflow.add_node("question_analysis", question_analyze)  # 질문 분석
+    workflow.add_node("additional_questions", question_clarify)  # 추가 질문
+    workflow.add_node("question_refinement", question_refine)  # 질문 구체화
+
+    workflow.add_conditional_edges(
+        "question_evaluation",
+        user_question_checker,
+        {
+            "0": "general_conversation",
+            "1": "question_analysis",
+        },
+    )
+
+    workflow.add_conditional_edges(
+        "question_analysis",
+        user_question_analyze_checker,
+        {
+            True: "additional_questions",
+            False: "question_refinement",
+        },
+    )
+
+    workflow.add_edge("additional_questions", "question_refinement")
+
+    # Set the entry point
+    workflow.set_entry_point("question_evaluation")
+
+    # Set up memory storage for recording
+    memory = MemorySaver()
+
+    # Compile the graph
+    app = workflow.compile(checkpointer=memory)
+
+    return app
